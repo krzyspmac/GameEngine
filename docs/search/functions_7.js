fa69dda95c8ba41f150f313e5e950b4d367fd19b
Krzysztof Pawłowski <krzyspmac@gmail.com>
2022-10-02 21:32:18 +0200

    Documentation update

--- docs/search/functions_7.js
+++ docs/search/functions_7.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['newstate_0',['newState',['../classengine_1_1_scripting_engine_i.html#a177b54dd0b47ab8dc22d498fca9bff38',1,'engine::ScriptingEngineI']]],
-  ['nudgedposition_1',['NudgedPosition',['../classengine_1_1_path_finder_data_i.html#a846331ef8c5a9b1d304d3f4b09aee691',1,'engine::PathFinderDataI']]]
+  ['nudgedposition_0',['NudgedPosition',['../classengine_1_1_path_finder_data_i.html#a846331ef8c5a9b1d304d3f4b09aee691',1,'engine::PathFinderDataI']]]
 ];
